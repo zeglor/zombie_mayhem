dee2c49080d9c6c02a0d9ea0e906af0565208cb8
Ivan Kharybin <zeglor@gmail.com>
2014-11-20 04:02:18 +0400

    added enemies

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os, pygame
 import math
 import numpy as np
+import random as r
 
 if not pygame.font: print('Warning, fonts disabled!')
 if not pygame.mixer: print('Warning, sound disabled!')
@@ -80,6 +81,26 @@ class Player():
 				self.weapon_cooldown = self.WEAPON_COOLDOWN
 				self.shooting_start_time = time
 
+class Enemy:
+	def __init__(self, start_pos=np.array([0,0]), speed=0):
+		self.pos = [float(pos_item) for pos_item in start_pos]
+		self.speed = float(speed)
+		self.coll_rect = pygame.Rect(np.array([float(pos_item) for pos_item in self.pos]) - [4,4], [9,9])
+		self.state = 'ALIVE'
+	
+	def update(self, player_pos):
+		'''
+		zombie needs to know where the player is to walk towards him and eat his brains
+		'''
+		dist = [b - a for a,b in zip(self.pos, player_pos)]
+		try:
+			angle = math.degrees(math.atan2(float(dist[1]), float(dist[0]))) % 360
+			speed = np.array([math.cos(math.radians(angle)), math.sin(math.radians(angle))]) * self.speed
+			self.pos += speed
+			self.coll_rect = pygame.Rect(np.array([float(pos_item) for pos_item in self.pos]) - [4,4], [9,9])
+		except ZeroDivisionError as e:
+			pass
+
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((640, 480,))
@@ -87,14 +108,18 @@ def main():
 	
 	player = Player(np.array([100, 100]), np.array([6, 9]), 5, "character.png")
 	speed = np.array([0,0])
+	zombie = Enemy(np.array([0,0]), 2)
 	
 	clock = pygame.time.Clock()
 	bullets = []
+	enemies = [zombie]
 	mapRect = pygame.Rect([0,0], [600, 400])
 	
 	while 1:
 		
 		clock.tick(60)
+		if r.random() > 0.99:
+			enemies.append(Enemy(np.array([10,0]), 2))
 		mouse_x, mouse_y = pygame.mouse.get_pos() 
 		#print(clock.get_fps())
 		
@@ -124,16 +149,26 @@ def main():
 		player.rotate((mouse_x, mouse_y))
 		player.move(speed)
 		
+		for enemy in enemies:
+			enemy.update(player.pos)
 		for bullet in bullets:
 			bullet.update()
 		player.update(bullets)
 		bullets[:] = [bullet for bullet in bullets if mapRect.contains(bullet.collRect)]
+		for enemy in enemies:
+			collIndx = enemy.coll_rect.collidelist([bullet.collRect for bullet in bullets])
+			if collIndx > -1:
+				enemy.state = 'DEAD'
+				del(bullets[collIndx])
+		enemies[:] = [enemy for enemy in enemies if enemy.state == 'ALIVE']
 					
 		screen.fill(bg_color)
 		pygame.draw.circle(screen, (0,0,255,), player.pos, 2)
 		pygame.draw.line(screen, (255,0,0,), player.pos, (mouse_x, mouse_y))
 		for bullet in bullets[:]:
 			pygame.draw.rect(screen, (255,0,0,), bullet.collRect, 5)
+		for enemy in enemies[:]:
+			pygame.draw.rect(screen, (0,255,0,), enemy.coll_rect)
 		screen.blit(player.image, player.drawing_point)
 		pygame.display.flip()
 
